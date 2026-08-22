6d26ddb79e9b225aa58c735f880f4d17ce3ec7bb
Sakshi-Gupta242 <203024585+Sakshi-Gupta242@users.noreply.github.com>
2026-08-22 18:38:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,18 +4,16 @@ public:
 
         if(n==0)
         return 0;
-
         if(n==1)
         return 1;
 
-        int a = 0;
-        int b = 1;
-
-        for(int i =2;i<=n;i++){
-        int c= a+b;
+        int a=0;
+        int b =1;
+        for(int i=2;i<=n;i++){
+        int c = a+b;
         a=b;
         b=c;
-    }
-    return b;
+        }
+        return b;
     }
 };
