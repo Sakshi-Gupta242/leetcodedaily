1936a07f519142a5f50375fc597da1c8002935f1
Sakshi-Gupta242 <203024585+Sakshi-Gupta242@users.noreply.github.com>
2026-09-12 14:46:01 +0530

    Time: 47 ms (62.68%), Space: 28.9 MB (99.31%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-
         int n = nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
 
-            if (i > 0 && nums[i] == nums[i-1])
-                continue;
-
-            int j = i + 1;
-            int k = n - 1;
-
-            while (j < k) {
-
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                }
-                else if (total < 0) {
-                    j++;
-                }
-                else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-
-                    j++;
-                    k--;
-
-                    while (j < k && nums[j] == nums[j-1])
+            int j = i+1,k=n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                    if(sum<0){
                         j++;
+                    }
+                        else if(sum>0){
+                            k--;
+                        }
 
-                    while (j < k && nums[k] == nums[k+1])
-                        k--;
+                        else{
+                            ans.push_back({nums[i],nums[j],nums[k]});
+                            j++;k--;
+                            while(j<k&& nums[j] ==nums[j-1])
+                            j++;
+                        }
+                    
+                    }
                 }
-            }
-        }
+            
+            return ans;
 
-        return res;
-    }
-};
+          }
+        };
