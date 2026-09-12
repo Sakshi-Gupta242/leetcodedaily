1c4e1a5570f0ca4dfde03d9b3b02df870768ab4e
Sakshi-Gupta242 <203024585+Sakshi-Gupta242@users.noreply.github.com>
2026-09-12 18:01:54 +0530

    Time: 47 ms (62.68%), Space: 29.2 MB (46.02%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>ans;
+        //sort array
         sort(nums.begin(),nums.end());
-
-        for(int i = 0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
-
-            int j = i+1,k=n-1;
+        // store ans
+        vector<vector<int>>ans;
+        //fix one element
+        for(int i = 0;i<nums.size();i++){
+            //skip duplicates i
+            if(i>0&& nums[i]==nums[i-1])
+            continue;
+            //two pointer
+            int j = i+1;
+            int k = nums.size()-1;
+            //search while j doesnot cross
             while(j<k){
+                //calculate sum
                 int sum = nums[i]+nums[j]+nums[k];
-                    if(sum<0){
-                        j++;
-                    }
-                        else if(sum>0){
-                            k--;
-                        }
-
-                        else{
-                            ans.push_back({nums[i],nums[j],nums[k]});
-                            j++;k--;
-                            while(j<k&& nums[j] ==nums[j-1])
-                            j++;
-                        }
-                    
-                    }
+                if(sum<0){
+                    j++;
                 }
-            
-            return ans;
-
-          }
-        };
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    //move both pointers
+                    j++;
+                    k--;
+                    //skip duplicates j
+                    while(j<k&&nums[j]==nums[j-1])
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+};
